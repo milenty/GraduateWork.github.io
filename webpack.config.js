9c40ee42767c555a853ad2b6e58833750dabc345
Samorukova Maria <milenty2012@yandex.ru>
2020-04-02 18:35:58 +0300

    3 commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // добавили плагин
+const WebpackMd5Hash = require('webpack-md5-hash'); 
+const ImageWebpackLoader = require("image-webpack-loader");
+const FileLoader = require("file-loader");// добавили плагин
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// подключаем плагин
+const isDev = process.env.NODE_ENV === 'development';
+// создаем переменную для development-сборки
+
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[chunkhash].js'
   },
+// указали путь к файлу, в квадратных скобках куда вставлять сгенерированный хеш
   module: {
     rules: [
       {
@@ -19,20 +27,50 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // добавили минификацию CSS
+        test: /\.(png|jpg|gif|ico|svg)$/,//мои изменения
+        use: [
+                'file-loader?name=./images/[name].[ext]', // указали папку, куда складывать изображения
+                {
+                        loader: 'image-webpack-loader',
+                        options: {}
+                },
+              ]
+      },
+      {
+        test: /\.css$/i,
+        use: [
+                        (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                        'css-loader', 
+                        'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]'
       }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({ // 
-      filename: 'style.[contenthash].css',
+        ]
+      },
+  plugins: [ 
+    new MiniCssExtractPlugin({
+        filename: 'style.[contenthash].css'
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+              preset: ['default'],
+      },
+      canPrint: true
     }),
     new HtmlWebpackPlugin({
       inject: false,
       template: './src/index.html',
       filename: 'index.html'
     }),
-    new WebpackMd5Hash()
+    new WebpackMd5Hash(
+    ), 
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
   ]
 };
